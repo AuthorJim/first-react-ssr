f563198dab7e9f0252df6a6843b3f90b3ec5670c
AuthorJim <1036760040@qq.com>
2018-10-14 17:17:48 +0800

    up 代码同构

--- src/index.js
+++ src/index.js
@@ -6,17 +6,16 @@ import { renderToString } from 'react-dom/server'
 import Home from './containers/Home'
 
 const app = express()
+app.use(express.static('public'))
 const content = renderToString(<Home />)
-// const content = ReactDom.render(<Home />, document.getElementById('root'))
 
 app.get('/', (req, res) =>
     res.send(`
     <html>
         <title>服务器端渲染</title>
         <body>
-            <div id="root">
-                ${content}
-            </div>
+            <div id="root">${content}</div>
+            <script src="/index.js"></script> 
         </body>
     </html>
 `)
